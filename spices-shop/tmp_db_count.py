4163d3cdf876cb0ff2ba9863dff1a2365299bb50
mahmudibrahimabba019-ops <mahmudibrahimabba019@gmail.com>
2026-09-03 12:37:57 +0100

    remove unnesary information

--- spices-shop/tmp_db_count.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://postgres:Mahmud019@localhost:5432/halari_spices')
-cur = conn.cursor()
-cur.execute("SELECT COUNT(*) FROM products")
-print(cur.fetchone())
-conn.close()
